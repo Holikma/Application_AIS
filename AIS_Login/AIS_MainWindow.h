dc9fb6fe62301ac0101192df900926386353e1f7
Holikma_ <marekholik01@gmail.com>
2024-01-05 15:26:54 +0100

    update

--- AIS_Login/AIS_MainWindow.h
+++ AIS_Login/AIS_MainWindow.h
@@ -9,13 +9,20 @@ class Subject {
 	private:
 		QString Name;
 		int Study_Year;
-		QString Study_Type;
+	public:
+		Subject(QString name, int study_year) { Name = name; Study_Year = study_year; };
+		QString Get_Name() { return Name; };
+		int Get_Study_Year() { return Study_Year; };
 };
 
 class Enrolled_Subject : public Subject {
 	private:
 		QString Mark;
 		int Attempts;
+	public:
+		Enrolled_Subject(QString name, int study_year, QString mark, int attempts) : Subject(name, study_year) { Mark = mark; Attempts = attempts; };
+		QString Get_Mark() { return Mark; };
+		int Get_Attempts() { return Attempts; };
 };
 
 class User {
@@ -24,7 +31,7 @@ class User {
 		QString Password;
 
 	public:
-		User(QString login, QString password);
+		User(QString login, QString password) { Login = login; Password = password; };
 		void Get_Info();
 };
 
@@ -35,15 +42,16 @@ class Student : public User {
 		int Year;
 
 	public:
-		Student(QString login, QString password, QMap<QString, QString> personal, QString access, int year, QVector<Subject> enrolled_subjects);
-		void Get_Enrolled_Subjects();
-		void Enroll_Subject(Subject subject);
+		Student(QString login, QString password, QMap<QString, QString> personal_info, int year) : User(login, password) { Personal_Info = personal_info; Year = year; };
+		QVector<Enrolled_Subject> Get_Enrolled_Subjects() { return Enrolled_Subjects; };
+		void Enroll_Subject(Subject subject) { Get_Enrolled_Subjects().push_back(Enrolled_Subject(subject.Get_Name(), subject.Get_Study_Year(), "0", 0)); };
 };
 
 class Phd_Student : public Student {
 	private:
 		QVector<Subject> Teaching_Subjects;
 	public:
+		void Modify_Marks(Student student, Subject subject, QString Mark) {}
 
 };
 
@@ -51,7 +59,7 @@ class Employee : public User {
 	private:
 		QString Position;
 	public:
-		void Get_Position();
+		QString Get_Position() { return Position; };
 };
 
 class Teacher : public Employee {
@@ -75,6 +83,7 @@ class AIS_MainWindow : public QMainWindow{
 		Ui::AIS_MainWindowClass ui;
 		QVector<User> Users;
 		QVector<Subject> Subjects;
+		QMap<Subject, QVector<Student>> Enrolled_Students;
 	public:
 		AIS_MainWindow(QWidget *parent = nullptr);
 		~AIS_MainWindow();
